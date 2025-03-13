be3ed122f342b24cc61483dbc4b45ce923a16df5
Christopher Loyd Nugent <skymage23.cn@gmail.com>
2025-03-13 18:57:06 -0500

    fixed an issue in "os_helper.py" where we were accidentally importing the "errors" Python standard library module when we meant to import the local "error" module.  Also, added ".gitignore" and set it to ignore Vim metadata files and ".pyc" file.

--- modules/os_utilities/os_helper.py
+++ modules/os_utilities/os_helper.py
@@ -1,9 +1,8 @@
 import os
-import errors
+from . import errors
 #We only support a handful of shells. So, make sure they exist on the system PATH
 
 def is_program_on_system_PATH(program_name):
-    #Hello
     for path in os.environ["PATH"].split(os.pathsep):
         if os.path.exists(os.path.join(path, program_name)):
             return True 
