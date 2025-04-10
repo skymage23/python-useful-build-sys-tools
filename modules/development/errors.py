cad4a4d30c2b63596bc750f85aefe062d92a677b
Christopher Loyd Nugent <skymage23.cn@gmail.com>
2025-04-10 14:05:31 -0500

    Fixed DevelopmentError to properly inherit from Exception.

--- modules/development/errors.py
+++ modules/development/errors.py
@@ -1,4 +1,4 @@
-class DevelopmentError():
+class DevelopmentError(Exception):
     def __init__(self, module_name: str, function_name:str, message:str):
         super().__init__(f"""This program has reached an irreconcilable state and has to exit.
 This is NOT your fault.  The developer made a mistake. If you have anyway
